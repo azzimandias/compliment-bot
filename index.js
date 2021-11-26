d1d93a6caa35402622304f8982930e18730cdf51
Andrew Sharanda <azzimandias@gmail.com>
2021-11-26 19:36:15 +0300

    cold fix

--- index.js
+++ index.js
@@ -100,9 +100,9 @@ const parseCorgisPhotos = async () => {
     const selector = await getHTML(`https://pixabay.com/ru/images/search/%D0%BA%D0%BE%D1%80%D0%B3%D0%B8/`)
     selector('.link--h3bPW').each((i, element) => {
         const imges = selector(element).find('img').attr('src')
-        console.log(selector(element))
         corgiPhotosArray.push(`${imges}`)
     })
+    console.log(selector.text())
 }
 
 async function sendCompliment(chatId) {
