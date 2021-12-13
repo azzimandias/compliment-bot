f5c6f4061fa63662b69572befcc978fff2a25105
Sofya <sofya@MacBook-Air-Sofya.local>
2021-12-13 19:21:37 +0300

    try new feature

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-    user: 'fxecnjmqfzlmhc',
+    /*user: 'fxecnjmqfzlmhc',
     password: process.env.DB_PASSWORD,
     database: 'dahcii52epf2v7',
     port: 5432,
-    host: 'ec2-52-209-246-87.eu-west-1.compute.amazonaws.com',
-    ssl: true
+    host: 'ec2-52-209-246-87.eu-west-1.compute.amazonaws.com',*/
+    uri: process.env.DB_URI,
+    //ssl: true
 })
 module.exports = pool
