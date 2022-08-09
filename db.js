fbb3f5ae5730d02e0dbfdb9f895f5a9ca974e461
Andrew Sharanda <andrewsharanda@MacBook-Pro-Andrew.local>
2022-08-09 16:36:23 +0300

    new db

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED='0'
 const Pool = require('pg').Pool
 const pool = new Pool({
-    user: 'fxecnjmqfzlmhc',
+    user: 'pgoicsyibypdze',
     password: process.env.DB_PASSWORD,
-    database: 'dahcii52epf2v7',
+    database: 'd7qck71212frgl',
     port: 5432,
-    host: 'ec2-52-209-246-87.eu-west-1.compute.amazonaws.com',
+    host: 'ec2-52-51-3-22.eu-west-1.compute.amazonaws.com',
     uri: process.env.DB_URI,
     ssl: true
 })
